20f8ffbdc1c9c4fdf44f6cc6a4c15f4cf7a44284
Manuel Reis <manuel.reis@schibsted.com>
2022-04-03 20:20:26 +0200

    Generated new .jar

--- src/test/java/no/mnemonic/senderapplication/SocketClientTest.java
+++ src/test/java/no/mnemonic/senderapplication/SocketClientTest.java
@@ -1,16 +1,11 @@
 package no.mnemonic.senderapplication;
 
-import no.mnemonic.senderapplication.domain.Message;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -20,24 +15,13 @@ class SocketClientTest {
     @SpyBean
     private SocketClient socketClient;
 
-    @Mock
-    private PrintWriter out;
-
-    @Mock
-    private BufferedReader in;
+    private final int PORT = 6667;
 
     @Test
     void testNoSocketConnection() throws IOException {
-        assertThrows(IOException.class, () -> socketClient.startConnection(6666));
+        assertThrows(IOException.class, () -> socketClient.startConnection(PORT));
 
         verify(socketClient, times(3))
-                .startConnection(6666);
-    }
-
-    @Test
-    void testSendMessageNoSocketConnection() {
-        var status = socketClient.sendMessage(new Message("test", "1", "test"));
-
-        assertThat(status).isFalse();
+                .startConnection(PORT);
     }
 }
